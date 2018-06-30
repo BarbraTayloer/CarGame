4de16cbbd4596dad6983e67a5b2619a08e7681dc
Norbó <hejjanorbo@gmail.com>
2018-06-30 18:36:21 +0200

    hi

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+    public void PlayBtn(string newGameLevel)
+    {
+        SceneManager.LoadScene(newGameLevel);
+    }
+
+    public void HighscoresBtn(string scoreTable)
+    {
+        SceneManager.LoadScene(scoreTable);
+    }
+
+    public void doExitGame()
+    {
+        Application.Quit();
+        Debug.Log("clicked exit");
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+    public Text counterText;
+    public float startTime;
+
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    private void Update()
+    {
+        float t = Time.time - startTime;
+
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f0");
+
+        counterText.text = minutes + ":" + seconds;
+    }
+
+    private void FinishLap()
+    {
+        float t = 0;
+    }
+}
+
